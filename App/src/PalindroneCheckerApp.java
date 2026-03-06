dcc631e8eeabe33165c55acafabefd996678620c
CodeWithOmG <omg720944@gmail.com>
2026-03-06 15:36:54 +0530

    Improve palindrome checking logic and variable names

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -2,19 +2,27 @@ public class PalindroneCheckerApp {
 
     public static void main(String[] args) {
 
-        String original = "madam";
+        String word = "madam";
 
-        String reversed = "";
+        char[] chars = word.toCharArray();
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-
-        // Compare original and reversed strings
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a palindrome.");
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
         } else {
-            System.out.println(original + " is not a palindrome.");
+            System.out.println(word + " is not a palindrome.");
         }
     }
 }
