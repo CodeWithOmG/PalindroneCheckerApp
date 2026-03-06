abba8ff75dc625ff9dd50acb211aa503b864947f
CodeWithOmG <omg720944@gmail.com>
2026-03-06 15:33:35 +0530

    UC3

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,17 +1,20 @@
 public class PalindroneCheckerApp {
+
     public static void main(String[] args) {
-        String word = "madam";
+
+        String original = "madam";
+
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Check palindrome using if-else
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a palindrome.");
+        // Compare original and reversed strings
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
         } else {
-            System.out.println(word + " is not a palindrome.");
+            System.out.println(original + " is not a palindrome.");
         }
     }
 }
