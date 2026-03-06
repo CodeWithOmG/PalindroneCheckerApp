ba457f2ca85983adaf8549a3999a3cd3bdac38b2
CodeWithOmG <omg720944@gmail.com>
2026-03-06 15:50:02 +0530

    UC6

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,21 +1,33 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class PalindroneCheckerApp {
+public class QueueStackPalindromeChecker {
 
     public static void main(String[] args) {
 
         String word = "madam";
-        Stack<Character> stack = new Stack<>();
 
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push to stack and enqueue to queue
         for (char c : word.toCharArray()) {
             stack.push(c);
-        }
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
+            queue.add(c);
         }
 
-        if (word.equals(reversed)) {
+        boolean isPalindrome = true;
+
+        // Compare stack pop (LIFO) with queue dequeue (FIFO)
+        while (!stack.isEmpty()) {
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
